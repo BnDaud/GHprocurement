9d764af262d4fe81201c072f3a2916c7d5698f59
BnDaudlinux <lawalsulaimon70@gmail.com>
2025-12-20 03:29:57 +0100

    change allowed origins to the actual origins

--- ghprocument/settings.py
+++ ghprocument/settings.py
@@ -167,7 +167,7 @@ DEFAULT_FILE_STORAGE = "cloudinary_storage.storage.MediaCloudinaryStorage"
 
 CORS_ALLOWED_ORIGINS = [
    "https://g-hprocurement-frontend.vercel.app",
-   "https://ghprocurement.vercel.app",
+   "https://www.ghprocurement.com",
     "http://localhost:3000",
     "http://192.168.0.129:3000",
     "http://127.0.0.1:3000",
