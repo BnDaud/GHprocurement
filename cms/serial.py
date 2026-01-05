869030f7d2f38d612dc43168f790cf9a193ab419
BnDaudlinux <lawalsulaimon70@gmail.com>
2026-01-05 11:42:24 +0100

    corrected user view and made user serial returns id

--- cms/serial.py
+++ cms/serial.py
@@ -9,7 +9,7 @@ class UserSerial(ModelSerializer):
     
     class Meta:
         model = User
-        fields = ["username",
+        fields = ["id","username",
                   "email",
                   "first_name",
                   "last_name",
