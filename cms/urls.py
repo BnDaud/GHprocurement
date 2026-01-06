83422e64b3e9d9f873cf4da947cb9d090e8bfbd8
BnDaudlinux <lawalsulaimon70@gmail.com>
2026-01-06 11:38:53 +0100

    included zoho api credentials in .env

--- cms/urls.py
+++ cms/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-
+from django.http import HttpResponse
 from .views import UserView ,  CatalogView , getTotalView , AllData , ServicesView, FAQView ,  EmailView,MetaDataView , RFQView
 from django.urls import path , include
 routes = DefaultRouter()
@@ -10,7 +10,14 @@ routes.register("faqs" ,FAQView , basename="FAQs")
 routes.register("metadata" , MetaDataView , basename="metadata")
 routes.register("rfqs", RFQView , basename="rfqs")
 
+
+
+
+def zoho_callback(request):
+    return HttpResponse(request.GET.get("code", "No code received"))
+
 urlpatterns = [path("gettotal" , getTotalView , name = "getTotal"),
 path("alldata/" , AllData , name = "alldata" ), 
-path("emails/" ,EmailView.as_view() )
+path("emails/" ,EmailView.as_view() ),
+  path("zoho/callback/", zoho_callback),
 ]+ routes.urls
